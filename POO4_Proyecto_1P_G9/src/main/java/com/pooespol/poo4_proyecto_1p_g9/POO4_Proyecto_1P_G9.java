f31e88c0bafade2d13ac278ddd987209d89b5737
Giovanni Sambonino <87047313+YuyoAndre@users.noreply.github.com>
2022-11-07 20:47:09 -0500

    subida1

---
+++ POO4_Proyecto_1P_G9/src/main/java/com/pooespol/poo4_proyecto_1p_g9/POO4_Proyecto_1P_G9.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.pooespol.poo4_proyecto_1p_g9;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class POO4_Proyecto_1P_G9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
