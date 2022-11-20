a39adf5544235433514114468c5d3b171a53c9cc
Giovanni Sambonino <87047313+YuyoAndre@users.noreply.github.com>
2022-11-20 10:36:53 -0500

    actualizacion

--- POO4_Proyecto_1P_G9/src/main/java/Usuario/Cliente.java
+++ POO4_Proyecto_1P_G9/src/main/java/Usuario/Cliente.java
@@ -23,25 +23,38 @@ public class Cliente extends Usuario {
     }
     
     public void comprarTicket(){
-        ArrayList<String> lecturaIntinerarios =ManejoArchivos.LeeFichero("itinerarios.txt");
-        lecturaIntinerarios.remove(0);
+        ArrayList<String> lecturaVuelos =ManejoArchivos.LeeFichero("vuelos.txt");
+        lecturaVuelos.remove(0);
         Scanner sc = new Scanner (System.in);
         System.out.println("---ORIGEN-----");
-        for (int i = 0; i < lecturaIntinerarios.size(); i++) {
-            String[] inti = lecturaIntinerarios.get(i).split(",");
-            System.out.println((i+1)+". "+inti[1]);
+        for (int i = 0; i < lecturaVuelos.size(); i++) {
+            String[] inti = lecturaVuelos.get(i).split(",");
+            System.out.println((i+1)+". "+inti[0]);
         }
         System.out.println("Elige punto de Partida: ");
         int pp = sc.nextInt();
         sc.nextLine();
+        String fo = "";
+        for (int i = 0; i < pp ; i++) {
+            String[] inti = lecturaVuelos.get(i).split(",");
+            fo = inti[2];
+        }
+                
         System.out.println("---DESTINO-----");
-        for (int i = 0; i < lecturaIntinerarios.size(); i++) {
-            String[] inti = lecturaIntinerarios.get(i).split(",");
-            System.out.println((i+1)+". "+inti[1]);
+        for (int i = 0; i < lecturaVuelos.size(); i++) {
+            String[] inti = lecturaVuelos.get(i).split(",");
+            System.out.println((i+1)+". "+inti[0]);
         }
         System.out.println("Elige punto de LLegada: ");
         int pll = sc.nextInt();
         sc.nextLine();
+        String fll = "";
+        for (int i = 0; i < pll ; i++) {
+            String[] inti = lecturaVuelos.get(i).split(",");
+            fll = inti[2];
+        }
+        System.out.println("FECHA DE SALIDA: " + fo);
+        System.out.println("FECHA DE RETORNO: "+ fll);
     }
     
     @Override
