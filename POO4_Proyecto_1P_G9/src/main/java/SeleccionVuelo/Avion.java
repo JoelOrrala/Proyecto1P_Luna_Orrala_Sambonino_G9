c3e31d2e2e6dd22347032650fabe830ae98d025b
Luis A. Luna A <laluna@espol.edu.ec>
2022-12-02 23:51:27 -0500

    Update del javadoc de la clase Itinerario

--- POO4_Proyecto_1P_G9/src/main/java/SeleccionVuelo/Avion.java
+++ POO4_Proyecto_1P_G9/src/main/java/SeleccionVuelo/Avion.java
@@ -1,5 +1,5 @@
-
 package SeleccionVuelo;
+
 import java.util.ArrayList;
 import manejoArchivos.ManejoArchivos;
 
@@ -9,85 +9,98 @@ import manejoArchivos.ManejoArchivos;
  */
 
 public class Avion {
+
     private String codigo;
     private int capacidad;
     private ArrayList<Asiento> listaAsiento;
-    
-/**
- * @param codigo codigo del avion
- * @param capacidad cantidad 
- */
+
+    /**
+     * @param codigo codigo del avion
+     * @param capacidad cantidad
+     */
     public Avion(String codigo, int capacidad) {
         this.codigo = codigo;
         this.capacidad = capacidad;
         this.listaAsiento = cargarAsientos();
     }
-    
-/**
- * Metodo get de la variable codigo
- * @return el codigo del avion como String
- */
+
+    /**
+     * Metodo get de la variable codigo
+     *
+     * @return el codigo del avion como String
+     */
     public String getCodigo() {
         return codigo;
     }
-    
-/**
- * Metodo set de la variable codigo
- * @param codigo Argumento para cambiar la variable codigo
- */
+
+    /**
+     * Metodo set de la variable codigo
+     *
+     * @param codigo Argumento para cambiar la variable codigo
+     */
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
+
+    /**
+     * Metodo get de la variable capacidad
+     *
+     * @return La capacidad del avion de tipo int
+     */
     
-/**
- * Metodo get de la variable capacidad
- * @return La capacidad del avion como int
- */
     public int getCapacidad() {
         return capacidad;
     }
+
+    /**
+     * Metodo set de la variable capacidad
+     *
+     * @param capacidad Argumento para cambiar la capacidad del avion
+     */
     
-/**
- * Metodo set de la variable capacidad 
- * @param capacidad Argumento para cambiar la capacidad del avion
- */
     public void setCapacidad(int capacidad) {
         this.capacidad = capacidad;
     }
+
+    /**
+     * Metodo get del ArrayList de objetos de la clase Asiento
+     *
+     * @return Una lista de asientos
+     */
     
-/**
- * Metodo get del ArrayList de objetos de la clase Asiento
- * @return Una lista de asientos
- */
     public ArrayList<Asiento> getListaAsiento() {
         return listaAsiento;
     }
+
+    /**
+     * Metodo set del ArrayList
+     *
+     * @param listaAsiento Lista de Objectos de la clase Asiento
+     */
     
-/**
- * Metodo set del ArrayList
- * @param listaAsiento Lista de Objectos de la clase Asiento
- */
     public void setListaAsiento(ArrayList<Asiento> listaAsiento) {
         this.listaAsiento = listaAsiento;
     }
+
+    /**
+     * Metodo para cargar los asientos del asientos.txt en un ArrayList
+     *
+     * @return ArrayList de objetos de la clase Asiento con la informacion
+     * sacada del asientos.txt
+     */
     
- /**
-  * Metodo para cargar los asientos del asientos.txt en un ArrayList
-  * @return ArrayList de objetos de la clase Asiento con la informacion
-  * sacada del asientos.txt
-  */   
-    private ArrayList<Asiento> cargarAsientos(){
+    private ArrayList<Asiento> cargarAsientos() {
         ArrayList<String> lecturaAsientos = ManejoArchivos.LeeFichero("asientos.txt");
         ArrayList<Asiento> listaAsiento = new ArrayList<>();
         lecturaAsientos.remove(0);
-        for (String linea: lecturaAsientos){
+        for (String linea : lecturaAsientos) {
             String[] datosAsiento = linea.split(",");
-            String codigoAvion = datosAsiento[0],numAsiento = datosAsiento[1];
+            String codigoAvion = datosAsiento[0], numAsiento = datosAsiento[1];
             Disponibilidad dispo = Disponibilidad.valueOf(datosAsiento[2]);
-            if (codigoAvion.equals(this.codigo)){
-                listaAsiento.add(new Asiento(codigoAvion,numAsiento,dispo));
+            if (codigoAvion.equals(this.codigo)) {
+                listaAsiento.add(new Asiento(codigoAvion, numAsiento, dispo));
             }
         }
-        return listaAsiento; 
+        return listaAsiento;
     }
 }
--- POO4_Proyecto_1P_G9/src/main/java/SeleccionVuelo/Itinerario.java
+++ POO4_Proyecto_1P_G9/src/main/java/SeleccionVuelo/Itinerario.java
@@ -1,20 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package SeleccionVuelo;
 
 /**
  *
  * @author Giovanni
  */
+
 public class Itinerario {
+
     private String ciudadOrigen;
     private String ciudadDestino;
     private String horaSalida;
     private String horallegada;
     private double duracion;
 
+    /**
+     * Constructor de la clase Itinerario
+     *
+     * @param ciudadOrigen Ciudad de donde parte el avion
+     * @param ciudadDestino Ciudad de donde llegará el avion
+     * @param horaSalida Hora del vuelo
+     * @param duracion Duracion del vuelo
+     */
+    
     public Itinerario(String ciudadOrigen, String ciudadDestino, String horaSalida, double duracion) {
         this.ciudadOrigen = ciudadOrigen;
         this.ciudadDestino = ciudadDestino;
@@ -22,44 +30,98 @@ public class Itinerario {
         this.duracion = duracion;
     }
 
+    /**
+     * Metodo get de la variable ciudadOrigen
+     * @return La ciudad de origen como String
+     */
+    
     public String getCiudadOrigen() {
         return ciudadOrigen;
     }
 
+    /**
+     * Metodo set de variable ciudadOrigen
+     * @param ciudadOrigen Argumento de tipo String para cambiar la ciudad de
+     * origen
+     */
+    
     public void setCiudadOrigen(String ciudadOrigen) {
         this.ciudadOrigen = ciudadOrigen;
     }
 
+    /**
+     * Metodo get de la variable ciudadDestino
+     * @return La ciudad de destino como tipo String 
+     */
+    
     public String getCiudadDestino() {
         return ciudadDestino;
     }
-
+    
+    /**
+     * Metodo set de la variable ciudadDestino
+     * @param ciudadDestino Argumento de tipo String para cambiar
+     * la ciudad de destino
+     */
+    
     public void setCiudadDestino(String ciudadDestino) {
         this.ciudadDestino = ciudadDestino;
     }
 
+    /**
+     * Metodo get de la variable horaSalida
+     * @return La hora de salida del vuelo
+     */
+    
     public String getHoraSalida() {
         return horaSalida;
     }
-
+    
+    /**
+     * Metodo set de la variable horaSalida
+     * @param horaSalida Argumento de tipo String para cambiar
+     * la hora de salida
+     */
+    
     public void setHoraSalida(String horaSalida) {
         this.horaSalida = horaSalida;
     }
 
+    /**
+     * Metodo get de la variable horaLlegada
+     * @return La hora de llegada del vuelo
+     */
     public String getHorallegada() {
         return horallegada;
     }
 
+    /**
+     * Metodo set de la variable horaLlegada
+     * @param horallegada Argumento de tipo String para cambiar
+     * la hora de llegada
+     */
+    
     public void setHorallegada(String horallegada) {
         this.horallegada = horallegada;
     }
 
+    /**
+     * Metodo get de la variable duracion
+     * @return La duracion del vuelo
+     */
+    
     public double getDuracion() {
         return duracion;
     }
 
+    /**
+     * Metodo set de la variable duracion
+     * @param duracion Argumento de tipo double para cambiar la
+     * duracion del vuelo
+     */
+    
     public void setDuracion(double duracion) {
         this.duracion = duracion;
     }
-    
+
 }
