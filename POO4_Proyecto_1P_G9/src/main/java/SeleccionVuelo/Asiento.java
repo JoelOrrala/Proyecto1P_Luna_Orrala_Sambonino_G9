ffe7efa147d058cc93f0c871257621b118ab9b5e
Luis A. Luna A <laluna@espol.edu.ec>
2022-12-02 21:04:38 -0500

    Update javadoc de la clase Asiento

--- POO4_Proyecto_1P_G9/src/main/java/SeleccionVuelo/Asiento.java
+++ POO4_Proyecto_1P_G9/src/main/java/SeleccionVuelo/Asiento.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package SeleccionVuelo;
 
 /**
@@ -12,33 +9,60 @@ public class Asiento {
     private String numAsiento;
     private Disponibilidad disponibilidad;
     private String codigoAvion;
+/**
+ * 
+ * @param codigoAvion dato del asiento
+ * @param numAsiento dato del asiento
+ * @param disponibilidad dato del asiento
+ */
 
     public Asiento(String codigoAvion,String numAsiento, Disponibilidad disponibilidad) {
         this.codigoAvion = codigoAvion;
         this.numAsiento = numAsiento;
         this.disponibilidad = disponibilidad;
     }
-
+    /**
+     * Metodo get de la variable numAsiento
+     * @return Devuelve el numero del asiento como String
+     */
     public String getNumAsiento() {
         return numAsiento;
     }
-
+/**
+ * Metodo set de la variable numAsiento que sirve para cambiar
+ * el numero de asiento
+ * @param numAsiento Recibe un String como numero de Asiento
+ */
     public void setNumAsiento(String numAsiento) {
         this.numAsiento = numAsiento;
     }
-
+/**
+ * Metodo get de la variable disponibilidad que devuelve
+ * la Disponibilidad del asiento que podria estar libre o ocupados
+ * @return Devuelve la disponibilidad del asiento
+ */
     public Disponibilidad getDisponibilidad() {
         return disponibilidad;
     }
-
+/**
+ * Metodo set de la variable disponibilidad que sirve para cambiar
+ * la disponibilidad del asiento
+ * @param disponibilidad Recibe un argumento de tipo Disponibilidad
+ */
     public void setDisponibilidad(Disponibilidad disponibilidad) {
         this.disponibilidad = disponibilidad;
     }
- 
+ /**
+  * Metodo get de la variable codigoAvion
+  * @return Devuelve el codigo del avion como String
+  */
     public String getCodigoAvion() {
         return codigoAvion;
     }
-    
+/**
+ * Metodo toString de la clase asiento sobreescrita de la clase Object
+ * @return Un String con la informacion del asiento
+ */
     @Override
     public String toString() {
         return "Asiento:" + "código avión = " + codigoAvion + ", número asiento = " + numAsiento + ", disponibilidad = " + disponibilidad + '}';
